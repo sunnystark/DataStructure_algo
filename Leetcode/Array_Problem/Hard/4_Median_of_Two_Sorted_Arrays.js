9603c4b22184b8d88337c8d22876a2bf3ac1161b
INCOFORGETECH\Sunny.Bhardwaj <Sunny.Bhardwaj@coforgetech.com>
2022-05-26 12:57:01 +0530

    Add two_sorted_array_problem file in hard folder

---
+++ Leetcode/Array_Problem/Hard/4_Median_of_Two_Sorted_Arrays.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+Share Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+ Example 1:
+ 
+ Input: nums1 = [1,3], nums2 = [2]
+ Output: 2.00000
+ Explanation: merged array = [1,2,3] and median is 2.
+ Example 2:
+ 
+ Input: nums1 = [1,2], nums2 = [3,4]
+ Output: 2.50000
+ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+  
+ Constraints:
+ 
+ nums1.length == m
+ nums2.length == n
+ 0 <= m <= 1000
+ 0 <= n <= 1000
+ 1 <= m + n <= 2000
+ -106 <= nums1[i], nums2[i] <= 106*/
+
+
+ var findMedianSortedArrays = function(nums1, nums2) {
+    let resulr, start, end, mid
+    let a = ([...nums1,...nums2])
+    let res =  a.sort((a,b) => a-b)
+    
+      if(res.length % 2 === 0){
+          mid = Math.floor(res.length / 2)
+          start = res.slice(0,mid) , end = res.slice(mid)
+          resulr = (start[start.length-1] + end[0])/2
+     } else{
+         mid = Math.floor(res.length / 2)
+         resulr = res[mid]
+     }
+    
+    
+   return resulr
+       
+   }
