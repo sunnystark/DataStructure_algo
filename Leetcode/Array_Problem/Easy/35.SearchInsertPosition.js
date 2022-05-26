70c32fac01223a12ebcaddb183c5d210f5a9c064
vipin chauhan <vip.chauhan23@gmail.com>
2022-05-26 18:58:39 +0530

    some array problems are solved

---
+++ Leetcode/Array_Problem/Easy/35.SearchInsertPosition.js
@@ -0,0 +1,29 @@
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4 */
+
+var searchInsert = function(nums, target) {
+    let insertedIndex = nums.length;   // return the length if the target is greater than all element from array 
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] > target){
+            insertedIndex = i;
+            break;
+        }else if(nums[i] === target){
+             insertedIndex = i;
+            break;
+        }
+    }
+    return insertedIndex;
+};
